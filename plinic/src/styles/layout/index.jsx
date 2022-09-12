31bedc59e86a1530eaba01cca5e4a173ac65bb79
Lami <Halam.Lee.Lami@gmail.com>
2022-09-13 02:37:25 +0900

    :fire::lipstick: (halam/delete/#35) Light & Dark mode 삭제

--- plinic/src/styles/layout/index.jsx
+++ plinic/src/styles/layout/index.jsx
@@ -1,34 +1,13 @@
-import React, { useState } from 'react';
 import styled from 'styled-components';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from '../../styles/theme';
-import { GlobalStyle } from '../GlobalStyles';
 
 import Header from './Header';
 import Main from './Main';
 
 function Layout({ children }) {
-  const localTheme = window.localStorage.getItem('theme') || 'light';
-  const [themeMode, setThemeMode] = useState(localTheme);
-  const themeObject = themeMode === 'light' ? lightTheme : darkTheme;
-
-  const toggleThemeMode = () => {
-    if (themeMode === 'light') {
-      setThemeMode('dark');
-      window.localStorage.setItem('theme', 'dark');
-    } else {
-      setThemeMode('light');
-      window.localStorage.setItem('theme', 'light');
-    }
-  };
-
   return (
     <Wrapper>
-      <ThemeProvider theme={themeObject}>
-        <GlobalStyle />
-        <Header themeMode={themeMode} toggleDarkMode={toggleThemeMode} />
-        <Main>{children}</Main>
-      </ThemeProvider>
+      <Header />
+      <Main>{children}</Main>
     </Wrapper>
   );
 }
@@ -37,8 +16,6 @@ export default Layout;
 
 const Wrapper = styled.div`
   height: 100vh;
-  /* background-color: ${({ theme }) => theme.color.navy}; */
-  /* color: white; */
   @media (max-width: 1100px) {
     width: 1024px;
     padding: 0 calc((1100px - 1024px) / 2);
