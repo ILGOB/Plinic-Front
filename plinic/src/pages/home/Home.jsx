0799e7ee4b0291aba854f6cbb6646f1bff337503
Lami <Halam.Lee.Lami@gmail.com>
2022-09-13 13:03:15 +0900

    :construction::lipstick: (halam/feature/homepage/#45) homepage 퍼블리싱 완료

--- plinic/src/pages/home/Home.jsx
+++ plinic/src/pages/home/Home.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 function Home() {
   return (
     <Wrapper>
       <Video autoPlay loop muted>
-        <source src="video/video1.mp4" type="video/mp4" />
+        {/* <source src="video/video1.mp4" type="video/mp4" /> */}
+        {/* <source src="video/video2.mp4" type="video/mp4" /> */}
+        {/* <source src="video/video3.mp4" type="video/mp4" /> */}
+        <source src="video/video4.mp4" type="video/mp4" />
       </Video>
-      ddd
+      {/* <LogoOverlay> */}
+      <PlinicLogo src="plinic_logo.png" />
+      {/* </LogoOverlay> */}
+      <LoginBtns>
+        <GoogleBtn>
+          <LogoImg src="img/google.png" />
+          <Text>Continue with Google</Text>
+        </GoogleBtn>
+        <KakaoBtn>
+          <LogoImg src="img/kakao.png" />
+          <Text>Continue with Kakao</Text>
+        </KakaoBtn>
+      </LoginBtns>
     </Wrapper>
   );
 }
 
 export default Home;
 
-const Wrapper = styled.div`
-  /* width: 100vw;
-  height: 100vh;
-  overflow: hidden; */
-`;
+const FLEX_CENTER_COLUMN = ({ theme }) => theme.align.flexCenterColumn;
+
+const Wrapper = styled.div``;
 
 const Video = styled.video`
   object-fit: cover;
@@ -28,4 +41,52 @@ const Video = styled.video`
   top: 0;
   left: 0;
   z-index: -1;
+`;
+
+const LogoOverlay = styled.div`
+  width: 500px;
+  height: 230px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.6);
+  ${FLEX_CENTER_COLUMN}
+`;
+
+const PlinicLogo = styled.img`
+  width: 407px;
+`;
+
+const LoginBtns = styled.div`
+  width: 100%;
+  height: fit-content;
+  ${FLEX_CENTER_COLUMN}
+  margin-top: 44px;
+`;
+
+const GoogleBtn = styled.div`
+  width: 300px;
+  height: 40px;
+  border-radius: 50px;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+`;
+
+const KakaoBtn = styled(GoogleBtn)`
+  background-color: rgba(250, 255, 0, 0.7);
+  margin-top: 24px;
+`;
+
+const LogoImg = styled.img`
+  position: absolute;
+  left: 13px;
+  top: 8px;
+`;
+
+const Text = styled.span`
+  ${({ theme }) => theme.font.weight.bold};
+  ${({ theme }) => theme.font.size[20]};
+  margin-left: 25px;
 `;
