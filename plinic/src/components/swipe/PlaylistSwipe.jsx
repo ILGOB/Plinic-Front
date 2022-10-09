635633b52f7bd625ffeb2a53fb9644e60f2dced9
taeyoung <rlaxodud911@gmail.com>
2022-10-10 00:40:51 +0900

    :dizzy: :lipstick: (taeyoung/refactor/playlistswipe/#105) 페이드 애니메이션 추가 및 보이는 카드 개수 변경

--- plinic/src/components/swipe/PlaylistSwipe.jsx
+++ plinic/src/components/swipe/PlaylistSwipe.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectCoverflow, Navigation } from 'swiper';
 import 'swiper/swiper.min.css';
@@ -31,7 +31,7 @@ function PlaylistSwipe() {
       scale: 1,
       rotate: 0,
       depth: 100,
-      stretch: -3,
+      stretch: 30,
       modifier: 2,
       slideShadows: false,
     },
@@ -57,31 +57,60 @@ export default PlaylistSwipe;
 const NAVY = ({ theme }) => theme.color.navy;
 const CENTER = ({ theme }) => theme.align.flexCenter;
 
+const FadeIn = isIn => keyframes`
+  0% {
+    opacity: ${isIn ? 0 : 1};
+  }
+  100% {
+    opacity: ${isIn ? 1 : 0};
+  }
+`;
+
 const Box = styled.div`
-  width: 400px;
+  width: 500px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  overflow: hidden;
+
+  .swiper-container-coverflow {
+    width: 500px;
+    right: 0px;
+  }
 
   .swiper-wrapper {
     width: 500px;
+    align-self: center;
   }
 
   .swiper-slide {
     width: 140px;
     height: 202px;
+    animation: ${FadeIn(false)} 0.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  }
+
+  .swiper-slide-prev,
+  .swiper-slide-active,
+  .swiper-slide-next {
+    animation: ${FadeIn(true)} 0.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
   }
 
   .swiper-button-prev,
   .swiper-button-next {
+    position: absolute;
     width: 40px;
     height: 40px;
     background: #fff;
     border: 1px solid ${NAVY};
     border-radius: 20px;
     color: ${NAVY};
-    ${CENTER}
+    ${CENTER};
+  }
+
+  .swiper-button-prev {
+    left: 0px;
+  }
+
+  .swiper-button-next {
+    right: 20px;
   }
 
   .swiper-button-prev::after {
