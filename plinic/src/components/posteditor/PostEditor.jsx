7e5d4dc77c52ab2af01ae6e42fad49eb51b9c6c1
taeyoung <rlaxodud911@gmail.com>
2022-10-11 10:06:02 +0900

    :zap: (taeyoung/feature/createpostpage/#83) 입력 길이 제한 추가

--- plinic/src/components/posteditor/PostEditor.jsx
+++ plinic/src/components/posteditor/PostEditor.jsx
@@ -6,8 +6,14 @@ import PlaylistSwipe from '../swipe/PlaylistSwipe';
 function PostEditor({ data, titleInput, setTitleInput, contentInput, setContentInput, setPlaylistInput }) {
   return (
     <Wrapper>
-      <Input usedFor={'title'} userInput={titleInput} setUserInput={setTitleInput} />
-      <Input usedFor={'content'} userInput={contentInput} setUserInput={setContentInput} />
+      <TitleWrapper>
+        <Input usedFor={'title'} userInput={titleInput} setUserInput={setTitleInput} maxLength={30} />
+        <LengthLabel isLong={titleInput.length === 30}>({titleInput.length}/30)</LengthLabel>
+      </TitleWrapper>
+      <ContentWrapper>
+        <Input usedFor={'content'} userInput={contentInput} setUserInput={setContentInput} maxLength={300} />
+        <LengthLabel isLong={contentInput.length === 300}>({contentInput.length}/300)</LengthLabel>
+      </ContentWrapper>
       <PlaylistSwipe data={data} setPlaylistInput={setPlaylistInput} />
     </Wrapper>
   );
@@ -18,4 +24,30 @@ export default PostEditor;
 const Wrapper = styled.div`
   ${({ theme }) => theme.align.flexCenterColumn}
   gap: 25px;
+`;
+
+const TitleWrapper = styled.div`
+  position: relative;
+
+  span {
+    position: absolute;
+    right: 12px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+
+  span {
+    position: absolute;
+    right: 12px;
+    bottom: 15px;
+  }
+`;
+
+const LengthLabel = styled.span`
+  ${({ theme }) => theme.font.size['16']};
+  color: ${props => (props.isLong ? ({ theme }) => theme.color.warning : ({ theme }) => theme.color.darkGray)};
 `;
