31c1096c1ad64e44ae2b2f6b0efedb71a48042d7
Lami <Halam.Lee.Lami@gmail.com>
2022-10-13 12:34:48 +0900

    :card_file_box: (halam/feature/post-page/#103) post 더미데이터 생성

---
+++ plinic/src/pages/post/dummyData.js
@@ -0,0 +1,26 @@
+const dummyData = [
+  {
+    // id는 임시로 추가한 데이터
+    id: 1,
+    is_updated: true,
+    created_at: '2022.10.05',
+    updated_at: '2022.10.06',
+    tag_set: ['우울', '집가고싶다', '에혀'],
+    playlist: {
+      title: '첫 번째 플레이리스트!',
+      thumbnail_img_url: '',
+      total_url: 'http://gdsanadev.com',
+      track_name: ['애국가 1절', '애국가 2절', '애국가 3절'],
+      genre_name: 'rock',
+      is_scrapped: true,
+      scrapper_name_set: ['goddessana', 'testuser2'],
+    },
+    liker_count: 0,
+    title: 'asdfa',
+    content: 'aaa',
+    is_like: false,
+    author: 'goddessana',
+  },
+];
+
+export default dummyData;
