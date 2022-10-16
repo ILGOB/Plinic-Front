f4f9a1d23587654cb32d5e78ed17b1cff77a962e
halamlee <halam.lee.lami@gmail.com>
2022-10-17 01:36:24 +0900

    :card_file_box: (halam/feature/playlist-page/#86) 더미데이터에 장르 추가

--- plinic/src/components/card/dummyData.js
+++ plinic/src/components/card/dummyData.js
@@ -3,18 +3,21 @@ const data = [
     id: 1,
     title: '그냥그냥',
     playlistNum: 10,
+    genre: 'acoustic',
     thumbnail: 'https://source.unsplash.com/random/1',
   },
   {
     id: 2,
     title: '신나',
     playlistNum: 5,
+    genre: 'disco',
     thumbnail: 'https://source.unsplash.com/random/2',
   },
   {
     id: 3,
     title: '졸려',
     playlistNum: 15,
+    genre: 'soundtracks',
     thumbnail: 'https://source.unsplash.com/random/3',
   },
 ];
