916fc4d62a181584ecd2011f89755ab89d839d8e
halamlee <halam.lee.lami@gmail.com>
2022-10-19 01:16:41 +0900

    :card_file_box: (halam/feature/post-page/#103) 더미데이터 수정

--- plinic/src/pages/post/dummyData.js
+++ plinic/src/pages/post/dummyData.js
@@ -1,26 +1,24 @@
-const dummyData = [
-  {
-    // id는 임시로 추가한 데이터
-    id: 1,
-    is_updated: true,
-    created_at: '2022.10.05',
-    updated_at: '2022.10.06',
-    tag_set: ['우울', '집가고싶다', '에혀'],
-    playlist: {
-      title: '첫 번째 플레이리스트!',
-      thumbnail_img_url: '',
-      total_url: 'http://gdsanadev.com',
-      track_name: ['애국가 1절', '애국가 2절', '애국가 3절'],
-      genre_name: 'rock',
-      is_scrapped: true,
-      scrapper_name_set: ['goddessana', 'testuser2'],
-    },
-    liker_count: 0,
-    title: 'asdfa',
-    content: 'aaa',
-    is_like: false,
-    author: 'goddessana',
+const dummyData = {
+  id: 1, // 게시물의 id
+  is_updated: true,
+  created_at: '2022.10.17',
+  updated_at: '2022.10.17',
+  tag_set: ['운동', '데드리프트', '광배근', '승모근', '대둔근'],
+  playlist: {
+    id: 1, // 변경 : 플레이리스트의 id
+    title: '등운동 할 때',
+    thumbnail_img_url: 'thumbnails/2022/10/17/육회.jpeg',
+    total_url: 'http://gdsanadev.com',
+    track_name: [],
+    genre_name: 'k-pop',
+    is_scrapped: true, // 변경 : 현재 로그인한 사람이 이 게시물을 스크랩했는가?
+    scrapper_count: 1, // 변경 : 이 플레이리스트를 스크랩한 사람의 수는 몇 명인가?
   },
-];
+  liker_count: 0,
+  title: '이거 원알엠 잴 때 들으면 좋아요!',
+  content: '진짜임..!',
+  is_like: false,
+  author: 'plinic',
+};
 
 export default dummyData;
