467efbf8ceb0cd1fe0c66a0f236a3ccfb5392543
taeyoung <rlaxodud911@gmail.com>
2022-10-20 14:29:15 +0900

    :zap: (taeyoung/feature/editor-component/#139) type을 받아 공지와 게시물에 맞는 에디터로 전환

---
+++ plinic/src/components/editor/Editor.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Input from '../input/Input';
+import styled from 'styled-components';
+import PlaylistSwipe from '../swipe/PlaylistSwipe';
+
+function Editor({ type, data, titleInput, setTitleInput, contentInput, setContentInput, setPlaylistInput }) {
+  const contentInputProps = { usedFor: 'content', userInput: contentInput, setUserInput: setContentInput };
+  return (
+    <Wrapper>
+      <Input usedFor={'title'} userInput={titleInput} setUserInput={setTitleInput} maxLength={30} />
+      <Input maxLength={type === 'post' && 300} {...contentInputProps} />
+      {type === 'post' && <PlaylistSwipe data={data} setPlaylistInput={setPlaylistInput} />}
+    </Wrapper>
+  );
+}
+
+export default Editor;
+
+const Wrapper = styled.div`
+  ${({ theme }) => theme.align.flexCenterColumn}
+  gap: 25px;
+`;
