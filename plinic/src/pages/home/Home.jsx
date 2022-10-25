a032cd9b3048c587c6d3f22fac679abf7bbc2ed0
halamlee <halam.lee.lami@gmail.com>
2022-10-26 00:08:32 +0900

    :sparkles::fire: (halam/beta): 첫페이지 카카오 로그인 삭제 및 클릭 시 마이페이지 이동

--- plinic/src/pages/home/Home.jsx
+++ plinic/src/pages/home/Home.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-
-// import dotenv from 'dotenv';
-// dotenv.config();
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 console.log(BASE_URL);
 
 function Home() {
   const [video, setVideo] = useState('');
-
-  const handleLogin = () => {
-    window.location.href = 'http://172.19.109.57:8000/api/v1/accounts/kakao-authentication/login';
-  };
 
   const getVideo = () => {
     axios.get(`${BASE_URL}/random-background/`).then(res => {
@@ -26,22 +20,25 @@ function Home() {
   }, []);
 
   return (
-    <Wrapper>
-      <Video autoPlay loop muted>
-        <source key={video} src={video} type="video/mp4" />
-      </Video>
-      <PlinicLogo src="plinic_logo.png" />
-      <BtnWrapper onClick={handleLogin}>
-        <Symbol src="img/symbol.png" />
-        <Text>Login with Kakao</Text>
-      </BtnWrapper>
-    </Wrapper>
+    <LinkStyled to={'/mypage'}>
+      <Wrapper>
+        <Video autoPlay loop muted>
+          <source key={video} src={video} type="video/mp4" />
+        </Video>
+        <PlinicLogo src="plinic_logo.png" />
+      </Wrapper>
+    </LinkStyled>
   );
 }
 
 export default Home;
 
 const FLEX_CENTER_COLUMN = ({ theme }) => theme.align.flexCenterColumn;
+
+const LinkStyled = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+`;
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -61,28 +58,4 @@ const Video = styled.video`
 
 const PlinicLogo = styled.img`
   width: 550px;
-`;
-
-const BtnWrapper = styled.div`
-  cursor: pointer;
-  width: 300px;
-  height: 50px;
-  border-radius: 12px;
-  background-color: #fee500;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
-
-const Symbol = styled.img`
-  width: 20px;
-  position: absolute;
-  left: 15px;
-`;
-
-const Text = styled.span`
-  ${({ theme }) => theme.font.weight.thick};
-  margin-left: 20px;
-  line-height: 1;
 `;
