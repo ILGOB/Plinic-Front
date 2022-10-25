1ae3473ab15c643e39983d91ff59d2062b3cb98b
halamlee <halam.lee.lami@gmail.com>
2022-10-26 00:12:17 +0900

    :lipstick::fire: (halam/beta) 헤더 로고만 있게 수정

--- plinic/src/styles/layout/Header.jsx
+++ plinic/src/styles/layout/Header.jsx
@@ -10,39 +10,15 @@ import { faMagnifyingGlass, faCircleUser } from '@fortawesome/free-solid-svg-ico
 function Header({ noMenu }) {
   const [isLogin, setIsLogin] = useRecoilState(loginAtom);
 
-  // const [isLogin, setIsLogin] = useState(false);
-
   const loginHandler = () => {
     setIsLogin(!isLogin);
   };
-
-  const loginHeader = (
-    <LinkStyle to="/search">
-      <LoginInfo>
-        <ProfileIcon icon={faCircleUser} />
-        <Nickname>Lami</Nickname>
-      </LoginInfo>
-    </LinkStyle>
-  );
-  const logoutHeader = <Button to="/">로그인</Button>;
 
   return (
     <Wrapper>
       <LinkStyle to={isLogin ? '/search' : '/'}>
         <img src={'/plinic_logo.png'} height={'60px'} />
       </LinkStyle>
-      <button onClick={loginHandler}>{isLogin ? 'logout' : 'login'}</button>
-      login state : {isLogin ? 'login' : 'logout'}
-      {noMenu || (
-        <Menu>
-          <LinkStyle to="/search">
-            <SearchIcon icon={faMagnifyingGlass} />
-          </LinkStyle>
-          <LinkStyle to="/posts">게시판</LinkStyle>
-          <DivideLine />
-          {isLogin ? loginHeader : logoutHeader}
-        </Menu>
-      )}
     </Wrapper>
   );
 }
@@ -64,49 +40,8 @@ const Wrapper = styled.div`
   position: relative;
 `;
 
-const Menu = styled.div`
-  width: fit-content;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  gap: 24px;
-`;
-
-const SearchIcon = styled(FontAwesomeIcon)`
-  font-size: 25px;
-  color: #6a6a6a;
-`;
-
-const DivideLine = styled.div`
-  width: 2px;
-  height: 30px;
-  background-color: #d9d9d9;
-`;
-
 const LinkStyle = styled(Link)`
   color: inherit;
   text-decoration: none;
   font-size: 20px;
-`;
-
-const Button = styled(LinkStyle)`
-  background-color: ${({ theme }) => theme.color.green};
-  color: ${({ theme }) => theme.color.white};
-  padding: 10px 16px;
-  border-radius: 12px;
-`;
-
-const LoginInfo = styled.div`
-  cursor: pointer;
-  ${FLEX_CENTER};
-  gap: 15px;
-`;
-
-const ProfileIcon = styled(FontAwesomeIcon)`
-  font-size: 30px;
-  color: ${NAVY};
-`;
-
-const Nickname = styled.span`
-  ${({ theme }) => theme.font.size[16]}
 `;
